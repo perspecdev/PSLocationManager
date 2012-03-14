a0a7c72e6a018345bd49820c25f217f2164ce220
Daniel Isenhower <daniel@perspecdev.com>
2012-03-14 19:03:36 +0800

    changed a bunch of #defines to static const

--- PSLocationManager/PSLocationManager.m
+++ PSLocationManager/PSLocationManager.m
@@ -12,20 +12,20 @@
 //  Want to use this code in your app?  Just send me a quick email about your project
 //  and I'll grant you an MIT-style license.
 
-#define kDistanceFilter 5 // the minimum distance (meters) for which we want to receive location updates (see docs for CLLocationManager.distanceFilter)
-#define kHeadingFilter 30 // the minimum angular change (degrees) for which we want to receive heading updates (see docs for CLLocationManager.headingFilter)
-#define kDistanceAndSpeedCalculationInterval 3 // the interval (seconds) at which we calculate the user's distance and speed
-#define kMinimumLocationUpdateInterval 10 // the interval (seconds) at which we ping for a new location if we haven't received one yet
-#define kNumLocationHistoriesToKeep 5 // the number of locations to store in history so that we can look back at them and determine which is most accurate
-#define kValidLocationHistoryDeltaInterval 3 // the maximum valid age in seconds of a location stored in the location history
-#define kNumSpeedHistoriesToAverage 3 // the number of speeds to store in history so that we can average them to get the current speed
-#define kPrioritizeFasterSpeeds 1 // if > 0, the currentSpeed and complete speed history will automatically be set to to the new speed if the new speed is faster than the averaged speed
-#define kMinLocationsNeededToUpdateDistanceAndSpeed 3 // the number of locations needed in history before we will even update the current distance and speed
-#define kRequiredHorizontalAccuracy 20.0 // the required accuracy in meters for a location.  if we receive anything above this number, the delegate will be informed that the signal is weak
-#define kMaximumAcceptableHorizontalAccuracy 70.0 // the maximum acceptable accuracy in meters for a location.  anything above this number will be completely ignored
-#define kGPSRefinementInterval 15 // the number of seconds at which we will attempt to achieve kRequiredHorizontalAccuracy before giving up and accepting kMaximumAcceptableHorizontalAccuracy
+static const NSUInteger kDistanceFilter = 5; // the minimum distance (meters) for which we want to receive location updates (see docs for CLLocationManager.distanceFilter)
+static const NSUInteger kHeadingFilter = 30; // the minimum angular change (degrees) for which we want to receive heading updates (see docs for CLLocationManager.headingFilter)
+static const NSUInteger kDistanceAndSpeedCalculationInterval = 3; // the interval (seconds) at which we calculate the user's distance and speed
+static const NSUInteger kMinimumLocationUpdateInterval = 10; // the interval (seconds) at which we ping for a new location if we haven't received one yet
+static const NSUInteger kNumLocationHistoriesToKeep = 5; // the number of locations to store in history so that we can look back at them and determine which is most accurate
+static const NSUInteger kValidLocationHistoryDeltaInterval = 3; // the maximum valid age in seconds of a location stored in the location history
+static const NSUInteger kNumSpeedHistoriesToAverage = 3; // the number of speeds to store in history so that we can average them to get the current speed
+static const NSUInteger kPrioritizeFasterSpeeds = 1; // if > 0, the currentSpeed and complete speed history will automatically be set to to the new speed if the new speed is faster than the averaged speed
+static const NSUInteger kMinLocationsNeededToUpdateDistanceAndSpeed = 3; // the number of locations needed in history before we will even update the current distance and speed
+static const CGFloat kRequiredHorizontalAccuracy = 20.0; // the required accuracy in meters for a location.  if we receive anything above this number, the delegate will be informed that the signal is weak
+static const CGFloat kMaximumAcceptableHorizontalAccuracy = 70.0; // the maximum acceptable accuracy in meters for a location.  anything above this number will be completely ignored
+static const NSUInteger kGPSRefinementInterval = 15; // the number of seconds at which we will attempt to achieve kRequiredHorizontalAccuracy before giving up and accepting kMaximumAcceptableHorizontalAccuracy
 
-#define kSpeedNotSet -1.0
+static const CGFloat kSpeedNotSet = -1.0;
 
 #import "PSLocationManager.h"
 
